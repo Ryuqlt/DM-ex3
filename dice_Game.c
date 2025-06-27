0628c4bfc86b343aeef2bf0dc1c1de1cea2766b6
Ryuqlt <s1320224@u-aizu.ac.jp>
2025-06-27 12:01:51 +0900

    Greetings added

--- dice_Game.c
+++ dice_Game.c
@@ -1,2 +1,13 @@
-kkkkkkkkkkkkk
-名前を聞くhennsyuu
+#include<stdio.h>
+int main(){
+
+  char name[50];
+  
+  printf("What is your name?\n");
+  scanf("%s",name);
+
+  printf("> %s\n",name);
+  printf("Hello, %s!\n",name);
+
+  return 0;
+}
