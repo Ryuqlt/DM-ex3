0cc9a1e6ea0c67fda4b5b0c5b0ca8a00c70350fc
Ryuqlt <s1320224@u-aizu.ac.jp>
2025-06-27 12:20:08 +0900

    Greetings added

--- dice_Game.c
+++ dice_Game.c
@@ -1,4 +1,11 @@
-  char name[50];
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+int x,y;
+int Total;
+char name[50];
   
   printf("What is your name?\n");
   scanf("%s",name);
