7b2610c69952e7c9a80c82a5fd389ece83d4db6a
Ryuqlt <s1320224@u-aizu.ac.jp>
2025-06-27 14:57:32 +0900

    Dice game:initial revision

--- dice_Game.c
+++ dice_Game.c
@@ -1,21 +1,21 @@
-srand(time(NULL));
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int x,y;
+  int Total;
+
+  srand(time(NULL));
   x=(rand() %6 +1);
   y=(rand() %6 +1);
 
   Total  = x+y;
-  
+
   printf("Rolling dice...\n");
   printf("Die1: %d\n",x);
   printf("Die2: %d\n",y);
 
   printf("Total value: %d\n",Total);
-  if(Total>=8){
-    printf("You won\n");
-  }
-  else{
-    printf("You lost\n");
-  }
-
-
-  return 0;
+return 0;
 }
