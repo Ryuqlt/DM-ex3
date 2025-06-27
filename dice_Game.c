e108b75bda63d659c2d44fff2fe99c5525860e21
Ryuqlt <s1320224@u-aizu.ac.jp>
2025-06-27 11:48:47 +0900

    Dice game:initial revision

--- dice_Game.c
+++ dice_Game.c
@@ -1,2 +1,22 @@
-kkkkkkkkkkkkk
-名前を聞くhennsyuu
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int x,y;
+  int Total;
+
+  srand(time(NULL));
+  x=(rand() %6 +1);
+  y=(rand() %6 +1);
+
+  Total  = x+y;
+  
+  printf("Rolling dice...\n");
+  printf("Die1: %d\n",x);
+  printf("Die2: %d\n",y);
+
+  printf("Total value: %d\n",Total);
+
+  return 0;
+}
